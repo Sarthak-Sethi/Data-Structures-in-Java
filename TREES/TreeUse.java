6502383667adcf03f19ffbff65d70ca438700632
Sarthak sethi <sarthaksethigithub@gmail.com>
2020-06-10 20:40:29 +0530

    Add files via upload

---
+++ TREES/TreeUse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TREES;
+
+/**
+ *
+ * @author sarthak sethi
+ */
+public class TreeUse {
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(4);
+        TreeNode<Integer> node1 = new TreeNode<>(2);
+        TreeNode<Integer> node2 = new TreeNode<>(3);
+        TreeNode<Integer> node3 = new TreeNode<>(5);
+        TreeNode<Integer> node4 = new TreeNode<>(6);
+      //  TreeNode<Integer> node5 = new TreeNode<>(6);
+        root.children.add(node1);
+        root.children.add(node2);
+        root.children.add(node3);
+        node3.children.add(node4);
+        // to check use debugger
+        
+        
+    }
+}
