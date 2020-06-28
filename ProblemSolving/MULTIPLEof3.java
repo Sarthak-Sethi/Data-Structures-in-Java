e2ffd67985f60e0c2451b4d9a34494e926a26750
Sarthak sethi <sarthaksethigithub@gmail.com>
2020-06-28 17:26:39 +0530

    Update MULTIPLEof3.java

--- ProblemSolving/MULTIPLEof3.java
+++ ProblemSolving/MULTIPLEof3.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ProblemSolving;
 import java.math.BigDecimal;
 import java.util.Scanner;
